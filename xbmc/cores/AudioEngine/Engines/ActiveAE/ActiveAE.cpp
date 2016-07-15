8a86319f404a37a8e9f93b515541d8e910e6ad70
Rainer Hochecker <fernetmenta@online.de>
2016-07-15 08:36:01 +0200

    AE: add ffmpeg atempo filter

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -1279,7 +1279,9 @@ void CActiveAE::Configure(AEAudioFormat *desiredFmt)
       }
       if (initSink && (*it)->m_processingBuffers)
       {
+        (*it)->m_processingBuffers->Flush();
         m_discardBufferPools.push_back((*it)->m_processingBuffers->GetResampleBuffers());
+        m_discardBufferPools.push_back((*it)->m_processingBuffers->GetAtempoBuffers());
         delete (*it)->m_processingBuffers;
         (*it)->m_processingBuffers = nullptr;
       }
@@ -1445,7 +1447,11 @@ void CActiveAE::DiscardStream(CActiveAEStream *stream)
       if ((*it)->m_inputBuffers)
         m_discardBufferPools.push_back((*it)->m_inputBuffers);
       if ((*it)->m_processingBuffers)
+      {
+        (*it)->m_processingBuffers->Flush();
         m_discardBufferPools.push_back((*it)->m_processingBuffers->GetResampleBuffers());
+        m_discardBufferPools.push_back((*it)->m_processingBuffers->GetAtempoBuffers());
+      }
       delete (*it)->m_processingBuffers;
       CLog::Log(LOGDEBUG, "CActiveAE::DiscardStream - audio stream deleted");
       m_stats.RemoveStream((*it)->m_id);
